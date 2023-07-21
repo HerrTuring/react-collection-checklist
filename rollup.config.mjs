3f35791e02af894084e871661a9c47d62d51d8a1
Herr Turing <admin@nda.software>
2023-07-20 22:08:06 -0300

    Added sourcemap to rollup.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -12,10 +12,12 @@ export default [{
   output: [{
     file: packageJson.main,
     format: 'cjs',
+    sourcemap: true,
   },
   {
     file: packageJson.module,
     format: 'esm',
+    sourcemap: true,
   }],
   plugins: [
     external(),
